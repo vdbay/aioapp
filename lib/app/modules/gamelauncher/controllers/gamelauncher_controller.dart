e8f51a84c57459a9c628c8ff8935c103718b3e14
VDBay <azharsubhan97@gmail.com>
2024-09-22 21:42:04 +0700

    :sparkles: improve much performance

--- lib/app/modules/gamelauncher/controllers/gamelauncher_controller.dart
+++ lib/app/modules/gamelauncher/controllers/gamelauncher_controller.dart
@@ -3,7 +3,6 @@ import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
 
 import '../../../utils/rootcommand.dart';
-// import '../../../utils/vsnackbar.dart';
 
 class GamelauncherController extends GetxController {
   RxList<String> packagesList = <String>[].obs;
@@ -24,19 +23,32 @@ class GamelauncherController extends GetxController {
   }
 
   Future<String?> runSelected(String packageName) async {
-    // await Vsnackbar.showVSnackbar('Please Wait', "Applying configuration...", vSnackBarType: VSnackBarType.info);
+    isLoading(true);
+    // force stop
     String? res = await RootCommand.run('am force-stop $packageName');
+    // set refresh rate
+    res = "$res\n${await RootCommand.run('settings put system peak_refresh_rate 60')}";
+    res = "$res\n${await RootCommand.run('settings put system min_refresh_rate 60')}";
+    res = "$res\n${await RootCommand.run('settings put system peak_refresh_rate 90')}";
+    res = "$res\n${await RootCommand.run('settings put system min_refresh_rate 90')}";
     res = "$res\n${await RootCommand.run('settings put system peak_refresh_rate 120')}";
     res = "$res\n${await RootCommand.run('settings put system min_refresh_rate 120')}";
-    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=2,downscaleFactor=0.7:mode=3,downscaleFactor=0.7,fps=60:mode=3,fps=60')}";
-    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=2,downscaleFactor=0.7:mode=3,downscaleFactor=0.7,fps=90:mode=3,fps=90')}";
-    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=2,downscaleFactor=0.7:mode=3,downscaleFactor=0.7,fps=120:mode=3,fps=120')}";
+    // set game overlay
+    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=1,downscaleFactor=0.7,fps=60,loadingBoost=28800000,useAngle=true:mode=2,downscaleFactor=0.7,fps=60,loadingBoost=28800000,useAngle=true:mode=3,downscaleFactor=0.7,fps=60,loadingBoost=28800000,useAngle=true')}";
+    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=1,downscaleFactor=0.7,fps=90,loadingBoost=28800000,useAngle=true:mode=2,downscaleFactor=0.7,fps=90,loadingBoost=28800000,useAngle=true:mode=3,downscaleFactor=0.7,fps=90,loadingBoost=28800000,useAngle=true')}";
+    res = "$res\n${await RootCommand.run('device_config put game_overlay $packageName mode=1,downscaleFactor=0.7,fps=120,loadingBoost=28800000,useAngle=true:mode=2,downscaleFactor=0.7,fps=120,loadingBoost=28800000,useAngle=true:mode=3,downscaleFactor=0.7,fps=120,loadingBoost=28800000,useAngle=true')}";
+    // set game performance
     res = "$res\n${await RootCommand.run('cmd game mode performance $packageName')}";
-    // res = "$res\n${await RootCommand.run('cmd power set-fixed-performance-mode-enabled true')}";
-    // res = "$res\n${await RootCommand.run('cmd package compile -m everything -f $packageName')}";
-    // await Vsnackbar.showVSnackbar('Success', "Enjoy!", vSnackBarType: VSnackBarType.success);
+    // clear ART JIT
+    res = "$res\n${await RootCommand.run('pm compile --reset $packageName')}";
+    res = "$res\n${await RootCommand.run('pm art clear-app-profiles $packageName')}";
+    res = "$res\n${await RootCommand.run('cmd package compile -m verify -f $packageName')}";
+    // force ART JIT
+    res = "$res\n${await RootCommand.run('cmd package compile -m speed -f $packageName')}";
+    // launch
     res = "$res\n${await RootCommand.run('monkey -p $packageName 1')}";
     res = "$res\n${await RootCommand.sendNotificationAndToast('Running and optimized!: $packageName')}";
+    isLoading(false);
     Get.back(closeOverlays: true);
     return res;
   }
